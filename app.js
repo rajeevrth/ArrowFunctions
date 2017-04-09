637ece2f95d112bb2a14f9bc7d721f06939adfe5
rajeevrth <rajeevkumarsharma@outlook.com>
2017-04-09 13:34:47 +0530

    passing argumnets as ...n

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ var arrowFunc = (x) => { return x * x; }
 
 console.log(func(2));
 console.log(arrowFunc(2));
+
+//Passing argumnets in arrow frunction
+
+var undefiniteFunc = (...n) => {
+    let result = 0;
+    for (let i = 0; i < n.length; i++) {
+        result = result + n[i];
+    }
+    return result;
+}
+
+console.log(undefiniteFunc(1, 6, 8, 9, 3, 4, 5, 6, 7, 8, 8, 9, 9));
